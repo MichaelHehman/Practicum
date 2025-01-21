82ed74f88c0626180d5cead0100763230a264ef3
Michael Hehman <hehmanml@mail.uc.edu>
2025-01-20 22:53:31 -0500

    Update ProductReader.java

--- ProductReader.java
+++ ProductReader.java
@@ -1,1 +1,19 @@
 
+public class Product {
+    private String ID;
+    private String name;
+    private String description;
+    private double cost;
+
+    public Product(String ID, String name, String description, double cost) {
+        this.ID = ID;
+        this.name = name;
+        this.description = description;
+        this.cost = cost;
+    }
+
+    public String toCSVDataRecord() {
+        return ID + ", " + name + ", " + description + ", " + cost;
+    }
+}
+
